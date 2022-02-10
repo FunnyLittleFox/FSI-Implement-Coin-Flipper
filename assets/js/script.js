c1b9786722500e146b4ef83153f24e7c38a8bd5e
Fox Flaherty <katyra14@gmail.com>
2022-02-09 21:33:24 -0800

    Working Penny Flipper

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,50 @@
 document.addEventListener('DOMContentLoaded', function () {
     // This is just a sanity check to make sure your JavaScript script is getting loaded
     // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+    console.log('Hi') // Sanity achieved
 
     // TODO: Add event listener and handler for flip and clear buttons
+    
+    let numberOfHeads = 0;
+    let numberOfTails = 0;
+    let total = numberOfHeads + numberOfTails;
 
+    const pennyButton = document.querySelector('#flip')
+
+    pennyButton.addEventListener('click', function(){
+        let headsFlip = Math.random() < 0,5;
+        if (headsFlip) {
+            document.getElementById('penny-image').src = './assets/image/penny-heads.jpg';
+            document.getElementById('message').textContent = 'You Flipped Heads!';
+            console.log('heads');
+            numberOfHeads++;
+        }
+        else{
+            document.getElementById('penny-image').src= './assets/images/penny-tails.jpg'
+            document.getElementById('message').textContent = 'You Flipped Tails!';
+            console.log('tails');
+            numberOfTails++;
+        }
+        
+        document.getElementsById ('heads').textContent = "numberOfHeads";
+        document.getElementsById ('tails').textContent = "numberOfTails";
+
+        if (total > 0) {
+        percentHeads = Math.round((numberOfHeads / total) * 100);
+        percentTails = Math.round((numberOfTails / total) * 100);
+        }
+
+        document.getElementById('clear').addEventListener('click', function(){
+        numberOfHeads = 0;
+        numberOfTails = 0;
+        total = 0;
+        })
+
+        document.getElementById('heads').textContent = numberOfHeads;
+        document.getElementById('tails').textContent = numberOfTails;
+        document.getElementById('heads-percent').textContent = `${percentofHeads}%`;
+        document.getElementById('tails-percent').textContent = `${percentofTails}%`;
+        document.getElementById('message').textContent = "Let\'s Get Rolling!";
     // Flip Button Click Handler
         // TODO: Determine flip outcome
         // TODO: Update image and status message in the DOM
